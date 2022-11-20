01064351e633f8ceddf98d911018318defc370e9
Killgame <ilgamkildiarov@yandex.ru>
2022-11-20 17:53:43 +0500

    Initial commit

---
+++ app/src/main/java/com/bam/resourceexample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.bam.resourceexample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        act_change.setOnClickListene()
+    }
+}
